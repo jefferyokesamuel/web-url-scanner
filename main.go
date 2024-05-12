8eb8a6f3f2bc41dba31e75f706eda2b454aafcd7
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-05-12 12:50:44 +0100

    updates

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func checkURL(url string) {
 	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Printf("%s is down\n", url)
+	} else {
+		defer resp.Body.Close()
+		fmt.Printf("%s -> status code is %d\n", url, resp.StatusCode)
+	}
+
 }
 
 func main() {
