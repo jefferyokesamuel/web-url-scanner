4dd45ed0a47a197fcb17704221f4413e9af0cb7f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-05-12 12:59:53 +0100

    updates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func checkURL(url string) {
@@ -13,6 +14,15 @@ func checkURL(url string) {
 	} else {
 		defer resp.Body.Close()
 		fmt.Printf("%s -> status code is %d\n", url, resp.StatusCode)
+		if resp.StatusCode == 200 {
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			file := strings.Split(url, "//")[1]
+			file += ".txt"
+
+			fmt.Printf("Writing URL body into %s\n", file)
+		}
+
+
 	}
 
 }
